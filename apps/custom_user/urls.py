0512af2fc362fd7dbc6e973e49901f66d3c32dc9
Maksym Tishchuk <58475629+MaksTishchuk@users.noreply.github.com>
2022-05-20 14:08:25 +0300

    Delete apps directory

--- apps/custom_user/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-
-urlpatterns = [
-    path('profile/<int:pk>/', views.MyUserView.as_view({'get': 'retrieve', 'put': 'update'})),
-    path('<int:pk>/', views.MyUserPublicView.as_view({'get': 'retrieve'})),
-]
